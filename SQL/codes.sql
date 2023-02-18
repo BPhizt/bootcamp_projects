3031056499cb3bb105d5564a389d36e7c17c58f2
Blue Nira <123177951+BPhizt@users.noreply.github.com>
2023-02-17 20:39:33 -0500

    Update codes.sql

--- SQL/codes.sql
+++ SQL/codes.sql
@@ -8,7 +8,7 @@ FROM tracks;
 
 
 -- subquery >> It will run inner first
--- outer query
+	-- outer query
 SELECT 
 				AVG(mb) AS avg_mb,
         SUM(mb) AS sum_mb,
@@ -22,3 +22,70 @@ FROM (
         bytes/ (1024*1024.0) AS mb
 	FROM tracks
 ) AS sub;
+
+
+-- subquery >> write version 1
+WITH sub AS (
+	SELECT * FROM customers
+	WHERE UPPER(country) = 'USA'
+)
+
+SELECT 
+	firstname, 
+    state, 
+    country, 
+    email
+FROM sub
+WHERE state IN ('NY', 'CA');
+
+-- version 2 >> the last line id differrent but the result/performance is the same
+SELECT 
+	firstname, 
+    state, 
+    country, 
+    email
+FROM (
+	SELECT * FROM customers
+	WHERE UPPER(country) = 'USA'
+) AS sub
+WHERE state = 'NY' OR state = 'CA';
+
+
+-- STRFTIME >> change formate date
+-- SQL doesn't concept about time/date so it contain the date as TEXT(string)
+WITH invoice_y2010 AS (
+		SELECT -- factInvoices
+      invoicedate,
+      STRFTIME('%Y%m', invoicedate) AS monthID,
+      customerid, -- key
+      total
+    FROM invoices
+    WHERE  STRFTIME('%Y', invoicedate) = '2010'
+), usa_customers AS ( -- dimCustomers
+		SELECT 
+			customerid, -- key
+			firstname, 
+			lastname 
+		FROM customers 
+		WHERE country = 'USA'
+)
+
+SELECT 
+		u.customerid, 
+    u.firstname, 
+    u.lastname, 
+    ROUND(SUM(i.total),2) AS total_invoice
+FROM usa_customers AS u
+JOIN invoice_y2010 AS i ON u.customerid = i.customerid
+GROUP BY u.customerid, u.firstname, u.lastname
+ORDER BY SUM(i.total) DESC -- descending high to low
+
+SELECT 
+		u.customerid, 
+    u.firstname, 
+    u.lastname, 
+    ROUND(SUM(i.total),2) AS total_invoice
+FROM usa_customers AS u
+JOIN invoice_y2010 AS i ON u.customerid = i.customerid
+GROUP BY 1,2,3
+ORDER BY 4 DESC -- descending high to low
